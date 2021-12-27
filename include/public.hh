bf295e53f7e73853e216dbaac48f7c94d08659e1
Cs144 <valentinmu@outlook.com>
2021-12-27 01:08:32 -0800

    add features

--- include/public.hh
+++ include/public.hh
@@ -1,16 +1,27 @@
 #ifndef PUBLIC
 #define PUBLIC
 #include <string>
-// server 和 client 公用
 
+// client & server message typecode protocal
 enum EnMsgType{
-    LOGIN_MSG = 1,
-    LOGIN_MSG_ACK,
+    LOGIN_MSG = 1, // 1
+    LOGIN_MSG_ACK, 
     REG_MSG ,
-    REG_MSG_ACK
+    REG_MSG_ACK ,
+    LOGOUT_MSG , // 5 
+    ADD_FRIEND_MSG,
+    CREATE_GROUP_MSG,
+    JOIN_GROUP_MSG,     
+    ONE_CHAT_MSG,
+    GROUP_CHAT_MSG // 10
 };
 
+
+// db definition
 const std::string online = "online";
 const std::string offline = "offline";
+
+const std::string creator = "creator";
+const std::string normies = "normal";
 
 #endif /* PUBLIC */
